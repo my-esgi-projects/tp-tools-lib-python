5cab9fe77a59cba813f7bd987364d8b88701e252
Bechir Delibes BKWEDOU-NGAMENI <b.bkwedou-ngameni@fr.clara.net>
2023-02-19 10:36:19 +0100

    feat: catch exception

--- workspace/server.py
+++ workspace/server.py
@@ -19,21 +19,26 @@ class Server:
 
 
 if __name__ == "__main__":
-    server = Server(port=8090)
-    server_socket = server.create_server_socket()
+    try:
+        server = Server(port=8090)
+        server_socket = server.create_server_socket()
 
-    while True:
-        client_socket, client_address = server_socket.accept()
-        pid = os.fork()
-        if pid == 0:
-            server_socket.close()
-            client_response = client_socket.recv(1024)
-            server_response = function_output(concat_choices=client_response.decode())
-            # send response of server to client
-            client_socket.send(b""+server_response.encode())
-            client_socket.close()
-            
-            print(client_response.decode())
-            os._exit(0)
-        else:
-            client_socket.close()
+        while True:
+            client_socket, client_address = server_socket.accept()
+            pid = os.fork()
+            if pid == 0:
+                server_socket.close()
+                client_response = client_socket.recv(1024)
+                server_response = function_output(concat_choices=client_response.decode())
+                # send response of server to client
+                client_socket.send(b""+server_response.encode())
+                client_socket.close()
+                
+                print(client_response.decode())
+                os._exit(0)
+            else:
+                client_socket.close()
+
+    except Exception as exception:
+        print(f"{exception}")
+        quit()
