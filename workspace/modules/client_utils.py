01a7ea94441d772f3ee058bbe86a9cef45baa3f8
Bechir Delibes BKWEDOU-NGAMENI <b.bkwedou-ngameni@fr.clara.net>
2023-02-21 17:11:07 +0100

    feat: clients utilities methods

--- workspace/modules/client_utils.py
+++ workspace/modules/client_utils.py
@@ -3,6 +3,7 @@
 
 from rich import print
 from rich.prompt import Prompt
+import argparse
 
 dict_menus = {
     "1": {
@@ -11,27 +12,24 @@ dict_menus = {
             "1": {
                 "label": "\t 1. Show os name",
                 "action": "os_name",
-                "final": "The os name is : "
+                "final": "The os name is : ",
             },
             "2": {
                 "label": "\t 2. Show os architecture",
                 "action": "os_architecture",
-                "final": "The architecture of os is : "
+                "final": "The architecture of os is : ",
             },
             "3": {
                 "label": "\t 3. Show os release",
                 "action": "os_release",
-                "final": "The release of os is : "
+                "final": "The release of os is : ",
             },
             "4": {
                 "label": "\t 4. Show os hostname",
                 "action": "os_hostname",
-                "final": "The hostname is : "
+                "final": "The hostname is : ",
             },
-            "0": {
-                "label": "\t 0. Back to main menu",
-                "action": "back_to_main_menu"
-            },
+            "0": {"label": "\t 0. Back to main menu", "action": "back_to_main_menu"},
         },
     },
     "2": {
@@ -40,17 +38,17 @@ dict_menus = {
             "1": {
                 "label": "\t 1. Show cpu name",
                 "action": "cpu_name",
-                "final": "CPU Name is : "
+                "final": "CPU Name is : ",
             },
             "2": {
                 "label": "\t 2. Show cpu architecture",
                 "action": "cpu_architecture",
-                "final": "CPU Architecture is : "
+                "final": "CPU Architecture is : ",
             },
             "3": {
                 "label": "\t 3. Percentage of cpu usage",
                 "action": "cpu_percentage_usage",
-                "final": "CPU Percentage used is : "
+                "final": "CPU Percentage used is : ",
             },
             "0": {
                 "label": "\t 0. Back to main menu",
@@ -59,44 +57,38 @@ dict_menus = {
         },
     },
     "3": {
-        "label": "3. Display Informations About Process",
+        "label": "3. Display Informations About Sensors",
         "submenu": {
             "1": {
-                "label": "\t 1. Show os process",
-                "action": "os_process"
+                "label": "\t 1. Show battery information",
+                "action": "sensors_battery",
+                "final": "Battery is : ",
             },
             "2": {
-                "label": "\t 2. Show application process",
-                "action": "application_process"
+                "label": "\t 2. Show sensors temperature",
+                "action": "sensors_temperature",
+                "final": "Temperature are : ",
             },
-            "0": {
-                "label": "\t 0. Back to main menu",
-                "action": "back_to_main_menu"
-            },
+            "0": {"label": "\t 0. Back to main menu", "action": "back_to_main_menu"},
         },
     },
     "4": {
         "label": "4. Display Informations About Memory",
         "submenu": {
             "1": {
-                "label": "\t 1. Show total memory size", 
+                "label": "\t 1. Show total memory size",
                 "action": "memory_total",
-                "final": "Total memory size in MB is : "
+                "final": "Total memory size in MB is : ",
             },
             "2": {
                 "label": "\t 2. Show memory available",
                 "action": "memory_available",
-                "final": "Memory available in MB is : "
+                "final": "Memory available in MB is : ",
             },
             "3": {
                 "label": "\t 3. Show used memory",
                 "action": "memory_used",
-                "final": "Memory used in MB is : "
-            },
-            "4": {
-                "label": "\t 4. Show used memory in percentage",
-                "action": "memory_used_percentage",
-                "final": "Percentage of memory used is : "
+                "final": "Memory used in MB is : ",
             },
             "0": {"label": "\t 0. Back to main menu", "action": "back_to_main_menu"},
         },
@@ -132,6 +124,14 @@ def display_submenu(user_menu, main_menu_choice):
     if main_menu_choice in list(user_menu.keys()):
         dict_submenu = user_menu[main_menu_choice]["submenu"]
 
-        return (main_menu_choice,display_menu(dict_submenu, "yellow"))
+        return (main_menu_choice, display_menu(dict_submenu, "yellow"))
     else:
-        return ("invalid","invalid")
+        return ("invalid", "invalid")
+
+
+def handle_args() -> tuple:
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument("-H", "--host", help="server listen address")
+    arg_parser.add_argument("-P", "--port", help="server listen port")
+    args = arg_parser.parse_args()
+    return (args, arg_parser)
