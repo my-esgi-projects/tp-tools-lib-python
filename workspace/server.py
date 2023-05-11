a619a8bb19b55b0cffcfae388ab938062033da13
Bechir Delibes BKWEDOU-NGAMENI <b.bkwedou-ngameni@fr.clara.net>
2023-05-11 21:19:29 +0200

    feat: add client request processing with threads

--- workspace/server.py
+++ workspace/server.py
@@ -3,13 +3,27 @@
 
 import socket
 import os
-import argparse
-from modules.server_utils import function_output
-from modules.client_utils import handle_args
+from threading import Thread
+from lib.server_utils import function_output
+from lib.client_utils import handle_args, default_port, default_host
 
+
+class ThreadWithReturnValue(Thread):
+    def __init__(self, group=None, target=None, name=None, args=(), kwargs=None, *, daemon=None):
+        Thread.__init__(self, group, target, name, args, kwargs, daemon=daemon)
+
+        self._return = None
+
+    def run(self):
+        if self._target is not None:
+            self._return = self._target(*self._args, **self._kwargs)
+
+    def join(self):
+        Thread.join(self)
+        return self._return
 
 class Server:
-    def __init__(self, host="", port=8090) -> None:
+    def __init__(self, host=default_host, port=default_port) -> None:
         self.host = host
         self.port = port
 
@@ -20,28 +34,25 @@ class Server:
         return server_socket
 
 
-def main(port=8090, host="") -> None:
+def main(port=default_port, host=default_host) -> None:
     try:
         server = Server(host=host, port=port)
         server_socket = server.create_server_socket()
 
         while True:
             client_socket, client_address = server_socket.accept()
-            pid = os.fork()
-            if pid == 0:
-                server_socket.close()
-                client_response = client_socket.recv(1024)
-                server_response = function_output(
-                    concat_choices=client_response.decode()
-                )
-                # send response of server to client
-                client_socket.send(b"" + server_response.encode())
-                client_socket.close()
-
-                # print(client_response.decode())
-                os._exit(0)
-            else:
-                client_socket.close()
+            client_response = client_socket.recv(1024)
+            choice = str(client_response.decode())
+        
+            # create new thread
+            thread = ThreadWithReturnValue(target=function_output, args=(choice,))
+            thread.start()
+            server_response = thread.join()
+            
+            # send response of server to client
+            client_socket.send(b"" + server_response.encode())
+    
+            client_socket.close()
 
     except Exception as exception:
         raise Exception(f"Error with socket connexion-->{str(exception)}")
@@ -51,18 +62,10 @@ if __name__ == "__main__":
     try:
         args, arg_parser = handle_args()
 
-        if args.host is None and args.port is None:
-            main()
-        else:
-            host = ""
-            port = 8090
-            if args.host is not None:
-                host = str(args.host)
+        host = default_host if args.host is None else str(args.host)
+        port = default_port if args.port is None else int(args.port)
 
-            if args.port is not None:
-                port = int(args.port)
-
-            main(host=host, port=port)
+        main(host=host, port=port)
 
     except Exception as exception:
         print(f"{str(exception)}")
