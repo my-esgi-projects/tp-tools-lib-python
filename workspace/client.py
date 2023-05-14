8542333cf899109463f267f1b091e7d4384029b1
Bechir Delibes BKWEDOU-NGAMENI <b.bkwedou-ngameni@fr.clara.net>
2023-05-14 10:31:33 +0200

    review: add spacing for improve readability

--- workspace/client.py
+++ workspace/client.py
@@ -3,9 +3,14 @@
 
 import socket
 from rich import print
-from lib.client_utils import display_menu, display_submenu, handle_args, default_host, default_port
+from lib.client_utils import (
+    display_menu,
+    display_submenu,
+    handle_args,
+    default_host,
+    default_port,
+)
 from lib.menu import dict_menus
-
 
 
 class Client:
@@ -22,7 +27,7 @@ class Client:
 
 
 def client_menu(host=default_host, port=default_port):
-    
+
     # initialise server
     server = (host, port)
 
@@ -32,17 +37,17 @@ def client_menu(host=default_host, port=default_port):
         if choice != "invalid":
             while True:
                 choices = display_submenu(dict_menus, main_menu_choice=choice)
-                
+
                 if choices[1] != "invalid":
                     if choices[1] == "0":
                         client_menu(host=host, port=port)
-                    
+
                     elif choices[1] == "yes":
                         quit()
-                    
+
                     elif choices[1] == "no":
                         client_menu(host=host, port=port)
-                    
+
                     else:
                         try:
                             client = Client(server)
@@ -55,7 +60,7 @@ def client_menu(host=default_host, port=default_port):
                         except Exception as exeption:
                             print(f"[bold red]{exeption}")
                             client_menu(host=host, port=port)
-                
+
                 else:
                     print("[bold magenta]Invalid choice.. Plz retry again")
 
@@ -68,10 +73,10 @@ if __name__ == "__main__":
 
     try:
         args, arg_parser = handle_args()
-        
+
         host = default_host if args.host is None else str(args.host)
         port = default_port if args.port is None else int(args.port)
-        
+
         client_menu(host=host, port=port)
 
     except Exception as exception:
